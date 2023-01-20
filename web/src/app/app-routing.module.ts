14a1817337f76f6f768af58a52f8e13b1efb4784
Zsomi4 <k723028128@ktch.hu>
2023-01-20 09:08:44 +0100

    Navbar hozzáadása

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'products', component: ProductlistComponent},
+  {path: '', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCommonModule} from '@angular/material/core';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DatasheetComponent } from './datasheet/datasheet.component';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { AdminmainComponent } from './adminmain/adminmain.component';
+import { NewproductComponent } from './newproduct/newproduct.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DatasheetComponent,
+    CopyrightComponent,
+    ProfileComponent,
+    AdminloginComponent,
+    AdminmainComponent,
+    NewproductComponent,
+    ProductlistComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCommonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
